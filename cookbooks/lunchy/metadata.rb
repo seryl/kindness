5d6544d8e0229f8ba5c7ffd2e40f3a89007e1688
Josh Toft <joshtoft@gmail.com>
2012-04-04 15:36:42 -0700

    Added the lunchy cookbook

---
+++ cookbooks/lunchy/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Josh Toft"
+maintainer_email "joshtoft@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures lunchy"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports         "mac_osx_x"
