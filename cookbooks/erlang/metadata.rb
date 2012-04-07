cd69f83e769983d3e833406de840b2ba3d072e74
Josh Toft <joshtoft@gmail.com>
2012-04-07 03:02:04 -0700

    Adding erlang cookbook

---
+++ cookbooks/erlang/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Josh Toft"
+maintainer_email "joshtoft@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures erlang"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
