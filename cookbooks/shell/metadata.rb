1d520a6e94919bf62acea37968ff3b151170b235
Josh Toft <joshtoft@gmail.com>
2012-04-09 00:58:18 -0700

    Adding shell cookbook

---
+++ cookbooks/shell/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Josh Toft"
+maintainer_email "joshtoft@gmail.com"
+license          "Apache v2.0"
+description      "Installs your user shell"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe           "default", "Enforces a selected shell for a user"
