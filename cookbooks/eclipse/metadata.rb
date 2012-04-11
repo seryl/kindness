2a75e449019b91457da87ef09e5cfa6fc4db745a
Josh Toft <joshtoft@gmail.com>
2012-04-10 18:06:31 -0700

    Added eclipse to cookbooks

---
+++ cookbooks/eclipse/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Josh Toft"
+maintainer_email "joshtoft@gmail.com"
+license          "Apache v2.0"
+description      "Installs eclipse"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ cookbooks/eclipse/recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Author:: Josh Toft <joshtoft@gmail.com>
+# Cookbook Name:: eclipse
+# Recipe:: default
+#
+# Copyright 2012, Josh Toft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if node['platform'] == 'mac_os_x'
+  bash "install eclipse" do
+    cwd "/opt"
+    code <<-EOH
+    curl -O http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/indigo/SR2/eclipse-jee-indigo-SR2-macosx-cocoa-x86_64.tar.gz&url=http://mirror.cc.vt.edu/pub/eclipse/technology/epp/downloads/release/indigo/SR2/eclipse-jee-indigo-SR2-macosx-cocoa-x86_64.tar.gz&mirror_id=504
+    tar -C /opt -zxvf eclipse-jee-indigo-SR2-macosx-cocoa-x86_64.tar.gz
+    rm eclipse-jee-indigo-SR2-macosx-cocoa-x86_64.tar.gz
+    EOH
+    not_if "ls /opt/eclipse | grep Eclipse"
+  end
+  
+  link "/Applications/Eclipse.app" do
+    to "/opt/eclipse/Eclipse.app"
+  end
+else
+end
+
+
