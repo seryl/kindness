57f304b560d6efc64ec0567a3ff902fad2361726
Josh Toft <joshtoft@gmail.com>
2012-08-20 11:09:12 -0700

    Removing homebrew recipe

--- cookbooks/homebrew/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             "homebrew"
-maintainer       "Josh Toft"
-maintainer_email "joshtoft@gmail.com"
-license          "Apache 2.0"
-description      "Install Homebrew and use it as your package provider in Mac OS X"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
-recipe           "homebrew", "Install Homebrew"
-supports         "mac_os_x"
