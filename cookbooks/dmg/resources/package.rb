d62b351b8cd0ccf2d48c8ce8fcf4b8a49687fa74
Josh Toft <joshtoft@gmail.com>
2013-06-06 12:30:06 -0600

    Update package.rb

--- cookbooks/dmg/resources/package.rb
+++ cookbooks/dmg/resources/package.rb
@@ -16,14 +16,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-actions :install, :upgrade
+actions :install
 
 attribute :app, :kind_of => String, :name_attribute => true
 attribute :source, :kind_of => String, :default => nil
+attribute :owner, :kind_of => String, :default => nil
 attribute :destination, :kind_of => String, :default => "/Applications"
 attribute :checksum, :kind_of => String, :default => nil
 attribute :volumes_dir, :kind_of => String, :default => nil
 attribute :dmg_name, :kind_of => String, :default => nil
 attribute :type, :kind_of => String, :default => "app"
 attribute :installed, :kind_of => [TrueClass, FalseClass], :default => false
-attribute :installed_resource, :kind_of => String, :default => nil
+attribute :package_id, :kind_of => String, :default => nil
+attribute :dmg_passphrase, :kind_of => String, :default => nil
+attribute :accept_eula, :kind_of => [TrueClass, FalseClass], :default => false
+
+def initialize(name, run_context=nil)
+  super
+  @action = :install
+end
